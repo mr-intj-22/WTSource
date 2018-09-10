218c90dbc0f0040342c3a186aef72eefbca1d460
abdulmalek shefat <abdulmalek.s.a.shefat@gmail.com>
2018-09-10 13:25:00 +0300

    added x-y option for graph.

--- app/src/main/java/dev/msl/wtmonitor/Graph/Graph.java
+++ app/src/main/java/dev/msl/wtmonitor/Graph/Graph.java
@@ -13,6 +13,7 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
+import android.support.v7.widget.AppCompatButton;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -27,6 +28,7 @@ import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.GridLabelRenderer;
 import com.jjoe64.graphview.LegendRenderer;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
@@ -44,16 +46,21 @@ import dev.msl.wtmonitor.R;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class Graph extends Fragment {
+public class Graph extends Fragment implements View.OnClickListener {
 
     @BindView(R.id.graph)
     GraphView graphView;
+    @BindView(R.id.x_label)
+    AppCompatButton xlable;
+    @BindView(R.id.y_label)
+    AppCompatButton ylable;
 
     private LineGraphSeries<DataPoint> series;
     private double graph2LastXValue = 0d, start = System.currentTimeMillis();
-    private String[] titles;
-    private int pos = 0;
+    private int xpos = 0, ypos = 1;
+    private String[] labels, units;
     private AlertDialog alert;
+    private int axis = 0;
 
 
     public Graph() {
@@ -76,25 +83,31 @@ public class Graph extends Fragment {
         if (getActivity() == null)
             return;
 
-        titles = getResources().getStringArray(R.array.digital_titles);
+        labels = getResources().getStringArray(R.array.digital_titles);
+        units = getResources().getStringArray(R.array.digital_units);
+
+        xlable.setOnClickListener(this);
+        ylable.setOnClickListener(this);
         series = new LineGraphSeries<>();
         // styling series
-        series.setTitle(titles[pos]);
+        series.setTitle(String.format(getString(R.string.graph_title), labels[ypos], labels[xpos]));
         graphView.getLegendRenderer().setVisible(true);
         graphView.getLegendRenderer().setBackgroundColor(Color.WHITE);
         graphView.getLegendRenderer().setTextColor(ContextCompat.getColor(getActivity(), R.color.secondary_text));
         graphView.getLegendRenderer().setAlign(LegendRenderer.LegendAlign.TOP);
+        graphView.getLegendRenderer().setFixedPosition(0, 0);
         series.setColor(ContextCompat.getColor(getActivity(), R.color.primary));
         series.setDrawDataPoints(true);
+        graphView.getGridLabelRenderer().setGridColor(Color.LTGRAY);
         series.setDrawBackground(true);
         series.setBackgroundColor(ContextCompat.getColor(getActivity(), R.color.accent_track));
-        series.setDataPointsRadius(10);
+        series.setDataPointsRadius(12);
         series.setThickness(8);
         graphView.addSeries(series);
         graphView.getViewport().setXAxisBoundsManual(true);
         graphView.getViewport().setMinX(0);
         graphView.getViewport().setMaxX(5);
-//        handler.postDelayed(runnable, 1000);
+        handler.postDelayed(runnable, 1000);
     }
 
     private Handler handler = new Handler();
@@ -113,14 +126,20 @@ public class Graph extends Fragment {
             AlertDialog.Builder alt_bld = new AlertDialog.Builder(getActivity());
             //alt_bld.setIcon(R.drawable.icon);
             alt_bld.setTitle(getString(R.string.plot_dialog_title));
-            alt_bld.setSingleChoiceItems(R.array.digital_titles, pos, new DialogInterface
+            alt_bld.setSingleChoiceItems(R.array.digital_titles, axis == 0 ? xpos : ypos, new DialogInterface
                     .OnClickListener() {
 
                 public void onClick(DialogInterface dialog, int item) {
-                    reset();
-                    series.setTitle(titles[item]);
-                    Graph.this.pos = item;
+                    if (axis == 0) {
+                        Graph.this.xpos = item;
+                        xlable.setText(String.format(getString(R.string.X), labels[xpos], units[xpos]));
+                    } else {
+                        Graph.this.ypos = item;
+                        ylable.setText(String.format(getString(R.string.Y), labels[ypos], units[ypos]));
+                    }
+                    series.setTitle(String.format(getString(R.string.graph_title), labels[ypos], labels[xpos]));
                     dialog.dismiss();// dismiss the alertbox after chose option
+                    reset();
                 }
             });
             alert = alt_bld.create();
@@ -129,42 +148,36 @@ public class Graph extends Fragment {
     }
 
     public void update(MonitoredData data) {
-        double d = 0d;
+        double x = getData(data, xpos), y = getData(data, ypos);
+
+        series.appendData(new DataPoint(x, y), true, 10);
+    }
+
+    private double getData(MonitoredData data, int pos) {
         switch (pos) {
             case 0:
-                d = data.getMotor_Speed();
-                break;
+                return data.getMotor_Speed();
             case 1:
-                d = data.getWind_Speed();
-                break;
+                return data.getWind_Speed();
             case 2:
-                d = data.getAttack_Angle();
-                break;
+                return data.getAttack_Angle();
             case 3:
-                d = data.getTarget_Pitot_Speed();
-                break;
+                return data.getTarget_Pitot_Speed();
             case 4:
-                d = data.getTemperature();
-                break;
+                return data.getTemperature();
             case 5:
-                d = data.getHumidity();
-                break;
+                return data.getHumidity();
             case 6:
-                d = data.getAltitude();
-                break;
+                return data.getAltitude();
             case 7:
-                d = data.getWeight_1();
-                break;
+                return data.getWeight_1();
             case 8:
-                d = data.getWeight_2();
-                break;
+                return data.getWeight_2();
             case 9:
-                d = data.getWeight_3();
-                break;
+                return data.getWeight_3();
+            default:
+                return (System.currentTimeMillis() - start) / 1000;
         }
-        graph2LastXValue = (System.currentTimeMillis() - start) / 1000;
-        Log.d("JSON", graph2LastXValue + "");
-        series.appendData(new DataPoint(graph2LastXValue, d), true, 10);
     }
 
     public void reset() {
@@ -174,18 +187,16 @@ public class Graph extends Fragment {
     }
 
     @Override
-    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-        super.onCreateOptionsMenu(menu, inflater);
-        inflater.inflate(R.menu.graph_menu, menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()){
-            case R.id.graph:
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.x_label:
+                axis = 0;
+                dialog();
+                break;
+            case R.id.y_label:
+                axis = 1;
                 dialog();
                 break;
         }
-        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/dev/msl/wtmonitor/Utils/BTUtils.java
+++ app/src/main/java/dev/msl/wtmonitor/Utils/BTUtils.java
@@ -1,23 +1,10 @@
 package dev.msl.wtmonitor.Utils;
 
 import android.bluetooth.BluetoothDevice;
-import android.bluetooth.BluetoothProfile;
-import android.content.Context;
-import android.content.DialogInterface;
-import android.support.v7.app.AlertDialog;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 
 import java.util.ArrayList;
-import java.util.Set;
 
 import dev.msl.wtmonitor.App;
-import dev.msl.wtmonitor.R;
-
-import static android.content.Context.LAYOUT_INFLATER_SERVICE;
 
 public class BTUtils {
 
@@ -32,15 +19,15 @@ public class BTUtils {
     }
 
     public static boolean isBTEnabled() {
-        return App.getBluetoothAdapter().isEnabled();
+        return false;//App.getBluetoothAdapter().isEnabled();
     }
 
     public static void enableBT() {
-        App.getBluetoothAdapter().enable();
+//        App.getBluetoothAdapter().enable();
     }
 
     public static void disableBT() {
-        App.getBluetoothAdapter().disable();
+//        App.getBluetoothAdapter().disable();
     }
 
     public static ArrayList<BluetoothDevice> getPairedDevices() {
